8c9182cc019f676d062ec20337d09ef81c080dfc
marambelhajrhouma <marambhr47@gmail.com>
2024-11-30 13:03:39 +0100

    version1

---
+++ projectflutter/lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:projectflutter/Provider/favorite_provider.dart';
+import 'package:projectflutter/Provider/quantity.dart';
+import 'package:provider/provider.dart';
+import 'Views/app_main_screen.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const MyApp());
+}
+// This widget is the root of your application.
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        // for favorite provider
+        ChangeNotifierProvider(create: (_)=>FavoriteProvider()),
+        // for quantity provider
+        ChangeNotifierProvider(create: (_) => QuantityProvider()),
+      ],
+      child: const MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: AppMainScreen(),
+      ),
+    );
+  }
+}
